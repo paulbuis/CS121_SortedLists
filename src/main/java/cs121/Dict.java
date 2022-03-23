84d33ca3ae05aef925e0edac1f532ba620c7ed2b
Paul Buis <00pebuis@bsu.edu>
2022-03-23 17:38:33 -0400

    Added comments

--- src/main/java/cs121/Dict.java
+++ src/main/java/cs121/Dict.java
@@ -1,7 +1,9 @@
 package cs121;
 
 
-
+// Dict is intended to be a subinterface of java.util.Set<String>
+// so if a class implements java.util.Set<String>
+// it will also implement Dict.
 public interface Dict extends SimpleSet<String> {
     boolean contains(String item);
 }
--- src/main/java/cs121/SimpleSet.java
+++ src/main/java/cs121/SimpleSet.java
@@ -2,7 +2,11 @@ package cs121;
 
 import java.util.Collection;
 
-public interface SimpleSet<T> {
+// SimpleSet<T> is intended to be a sub-interface
+// of java.util.Set<T>.
+// In particular, if a class implements java.util.Set<String>
+// it can also trivially implement SimpleSet<String>
+public interface SimpleSet<T> extends Iterable<T> {
     boolean add(T item);
     boolean addAll(Collection<? extends T> collection);
     int size();
